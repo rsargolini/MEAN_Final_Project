a0cc9cd192dcadc4ba8ba9a86db73dd62ba246a3
Ray Sargolini <52667551+rsargolini@users.noreply.github.com>
2019-10-02 08:59:38 -0400

    Update user.model.ts

--- client/src/app/models/user.model.ts
+++ client/src/app/models/user.model.ts
@@ -6,11 +6,13 @@
     public emailAddress: string = '';
     // Password for demo purposes only!
     public password: string = '';
-    
-    constructor(userId: number, userName: string, emailAddress: string, password: string) {
+    public is_admin: number;
+
+    constructor(userId: number, userName: string, emailAddress: string, password: string, is_admin: number) {
       this.userId = userId;
       this.userName = userName;
       this.emailAddress = emailAddress;
       this.password = password;
+      this.is_admin = is_admin;
     }
   }
